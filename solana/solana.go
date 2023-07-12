e32386cc9efa7f586ff0037652a8a9ec0a7b3ac5
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-07-11 21:18:35 -0400

    Add new method

--- solana/solana.go
+++ solana/solana.go
@@ -4,15 +4,17 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 type RequestBody struct {
-	Jsonrpc string   `json:"jsonrpc"`
-	ID      int64    `json:"id"`
-	Method  string   `json:"method"`
-	Params  []string `json:"params"`
+	Jsonrpc string      `json:"jsonrpc"`
+	ID      int64       `json:"id"`
+	Method  string      `json:"method"`
+	Params  []ParamsOpt `json:"params"`
 }
 
 type ResponseBody struct {
@@ -24,6 +26,20 @@ type ResponseBody struct {
 type Result struct {
 	Context interface{} `json:"context"`
 	Value   uint64      `json:"value"`
+}
+
+type GetLatestBlockHashResponse struct {
+	Jsonrpc string                   `json:"jsonrpc"`
+	Result  GetLatestBlockHashResult `json:"result"`
+	ID      uint64                   `json:"id"`
+}
+
+type GetLatestBlockHashResult struct {
+	Context interface{} `json:"context"`
+	Value   struct {
+		Blockhash            string `json:"blockhash"`
+		LastValidBlockHeight uint64 `json:"lastValidBlockHeight"`
+	}
 }
 
 // global variable
@@ -44,7 +60,9 @@ func Connect(s string) error {
 }
 
 func GetBalance(s string) (uint64, error) {
-	var a = RequestBody{"2.0", 1, "getBalance", []string{s}}
+	var a = RequestBody{"2.0", 1, "getBalance", []ParamsOpt{
+		{"processed"},
+	}}
 	jsonBody, err := json.Marshal(&a)
 	if err != nil {
 		return 0, err
@@ -64,6 +82,39 @@ func GetBalance(s string) (uint64, error) {
 	return r.Result.Value, nil
 }
 
+type ParamsOpt struct {
+	Commitment string `json:"commitment"`
+}
+
 func GetLatestBlockHash() uint64 {
 	return 0
+}
+
+func GetAccountInfo() {
+	a := RequestBody{"2.0", 1, "getLatestBlockhash", []ParamsOpt{
+		{"processed"},
+	}}
+
+	jsonBody, err := json.Marshal(a)
+	if err != nil {
+		log.Fatal("v ...any")
+	}
+
+	resp, err := http.Post(rpc_cluster, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		log.Fatal("sdasd")
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("asdasdad")
+	}
+
+	var r GetLatestBlockHashResponse
+	err = json.Unmarshal(data, &r)
+	if err != nil {
+		fmt.Println("Cannot unmarshal")
+	}
+
+	fmt.Printf("Data: %v", r)
 }
